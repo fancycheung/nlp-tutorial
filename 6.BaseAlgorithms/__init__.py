cd524e1a28d9d493e45b88231eccc893a34096bf
fancy <fancy.zhang@ximalaya.com>
2019-03-14 11:22:21 +0800

    增加基础算法

---
+++ 6.BaseAlgorithms/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+@author:nali 
+@file: __init__.py.py 
+@time: 2019/3/14/上午11:20
+@software: PyCharm
+"""
+
+"""
+基础算法
+"""
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+if __name__ == "__main__":
+    pass
